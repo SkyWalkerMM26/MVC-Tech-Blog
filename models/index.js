e3f39547ae15c6dbf276a42bf9c9c4c06ceb8a30
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-08 21:40:13 -0800

    create relationships for the models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,36 @@
-const Users = require('./User');
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-module.exports = { Users };
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+    hooks:true
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+    hooks: true
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+    hooks:true
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+    hooks:true
+})
+
+module.exports = { User, Post, Comment };
