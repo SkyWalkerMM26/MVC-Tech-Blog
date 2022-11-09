ef2596234fad715bd2a3555e0ca7872e571cd26c
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-08 23:11:48 -0800

    create comment-seeds.js

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,18 @@
+const { Comment } = require('../models');
+
+const commentData = [
+  {
+    comment_text: "Wow! ",
+    post_id: 3,
+    user_id: 1
+  },
+  {
+    comment_text: "This is amazing!",
+    post_id: 1,
+    user_id: 4
+  },
+];
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
