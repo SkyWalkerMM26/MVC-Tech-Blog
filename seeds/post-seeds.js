731b9239c07fa40e2abfd0d6e1c9907b7d5ac921
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-08 23:21:00 -0800

    create post-seeds.js

---
+++ seeds/post-seeds.js
@@ -0,0 +1,19 @@
+const { Post } = require('../models');
+
+const postData = [
+  {
+    title: 'Model',
+    post_text: 'Model updates the view for users in the MVC paradigm',
+    user_id: 1,
+  },
+  {
+    title: 'Controller',
+    post_text: 'The controller files can manipulates the model data.',
+    user_id: 4,
+  },
+
+]
+
+const seedPosts = () => Post.bulkCreate(postData);
+
+module.exports = seedPosts;
